21a039fea3daf5e693574a1b8c95ffad509829a9
Juan Santana <juanka9755@hotmail.com>
2023-10-11 07:45:20 -0500

    07 SIMPLE BANK
    Se crean queries de user
    CreateUser y GetUser
    
    Se Crea test de sqlc de user
    
    Se crea función para generar email random
    Se actualiza mock de User
    
    Se actualiza función e crear cuenta, para user relación con user

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	db "github.com/jnka9755/go-07SIMPLE-BANK/db/sqlc"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -44,6 +45,14 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	account, err := server.store.CreateAccount(ctx, arg)
 
 	if err != nil {
+
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
