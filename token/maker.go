5ba7bdaa4a1d6e921dace5fbd5b0311f767cd340
Juan Santana <juanka9755@hotmail.com>
2023-10-25 09:23:41 -0500

    07 - SIMPLE BANK
    Se implementa JWT y PASETO
    
    Se crean clases para manejo de autorización
    Se crea pruebas unitarias para cada implementación

---
+++ token/maker.go
@@ -0,0 +1,15 @@
+package token
+
+import (
+	"time"
+)
+
+// Maker is an interface for mananging tokens
+type Maker interface {
+
+	//Create creates a new token for a specific username and duration
+	CreateToken(username string, duration time.Duration) (string, error)
+
+	//VerifyToken checks if the token is valid or not
+	VerifyToken(token string) (*Payload, error)
+}
