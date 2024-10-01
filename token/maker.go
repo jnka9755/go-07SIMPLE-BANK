89ed81c3ace3c9f2998bcf6cb7209c8f62b35806
Juan Santana <juanka9755@hotmail.com>
2024-10-01 18:17:06 -0500

    Add refresh token
    Se añade README
    se implementa refresh token session
    se agrega nueva migration (sessions)
    se ajustan pruebas unitarias
    se crea nuevo sqlc de session
    se agrega nueva variable de entorno

--- token/maker.go
+++ token/maker.go
@@ -8,7 +8,7 @@ import (
 type Maker interface {
 
 	//Create creates a new token for a specific username and duration
-	CreateToken(username string, duration time.Duration) (string, error)
+	CreateToken(username string, duration time.Duration) (string, *Payload, error)
 
 	//VerifyToken checks if the token is valid or not
 	VerifyToken(token string) (*Payload, error)
